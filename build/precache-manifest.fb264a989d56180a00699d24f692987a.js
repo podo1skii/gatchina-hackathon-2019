9f1111b0ed4885b961692cc2a6fc8ce2b51884e0
Max <dragoonz@yandex.ru>
2019-05-26 03:26:22 +0300

    otkat

---
+++ build/precache-manifest.fb264a989d56180a00699d24f692987a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27834d2dac2a986ed1647f80f6752a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c94951e294ee0e0ea09d",
+    "url": "/static/css/main.68b13888.chunk.css"
+  },
+  {
+    "revision": "bdd9008d9ae68ab8a01f",
+    "url": "/static/js/2.21532fda.chunk.js"
+  },
+  {
+    "revision": "c94951e294ee0e0ea09d",
+    "url": "/static/js/main.2f120402.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4ad13760bea2f998bb90668ef8ea611e",
+    "url": "/static/media/bank-2.4ad13760.svg"
+  },
+  {
+    "revision": "4de1539e055f71f488e020bba18bc0b4",
+    "url": "/static/media/bank.4de1539e.svg"
+  },
+  {
+    "revision": "a9501241e7119bb4a7ff4eccb142be00",
+    "url": "/static/media/bike.a9501241.svg"
+  },
+  {
+    "revision": "dd9312939c5e47f6bb6fba02fc72b7c7",
+    "url": "/static/media/burger.dd931293.svg"
+  },
+  {
+    "revision": "4f31b277af149919db3bdd9c10b47fc3",
+    "url": "/static/media/greek-column.4f31b277.svg"
+  },
+  {
+    "revision": "d201be69bceb43f53fd848344cf4e8ce",
+    "url": "/static/media/like-icon.d201be69.svg"
+  },
+  {
+    "revision": "8a1767241c4a91e9b1544dbb59eec804",
+    "url": "/static/media/pizza.8a176724.svg"
+  },
+  {
+    "revision": "5f18caafdeb62802fd95cde0875b59a5",
+    "url": "/static/media/restaurant.5f18caaf.svg"
+  },
+  {
+    "revision": "29398a7a28afc16d0b725667bfba5d33",
+    "url": "/static/media/salad.29398a7a.svg"
+  },
+  {
+    "revision": "6a7a8fec692b3b344d677c360191299c",
+    "url": "/static/media/sports-car.6a7a8fec.svg"
+  },
+  {
+    "revision": "68da4d7ca48c205a5682c0f2596c3947",
+    "url": "/static/media/sushi-dish.68da4d7c.svg"
+  },
+  {
+    "revision": "cd71cf0fb98aee49c504a46af4d5ade5",
+    "url": "/static/media/up-arrow.cd71cf0f.svg"
+  }
+]);
